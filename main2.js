ec6634cff8600a6633e05f1fe85644c93def465a
nazar9991 <118369899+nazar9991@users.noreply.github.com>
2023-03-29 18:25:55 +0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,33 @@
+
+const fs = require('fs');
+const readStream = fs.createReadStream('t[t.file', { encoding: 'utf-8'});
+
+
+
+// task 1
+  readStream.on('data', (chunk) => {
+    console.log(chunk);
+  });
+  readStream.on('end', () => {
+    console.log('file reading is complete');
+  });
+
+
+
+// task 2
+    readStream.on('data', (chunk) =>{
+    const count = chunk.toString().split(" ").length
+        console.log ('words in the file:', count)
+     })
+
+
+
+// task *
+let buffer = '';
+let wordCount = 0;
+readStream.on('data', chunk => {
+    buffer += chunk;
+    const words = buffer.toString().split(' ');
+    buffer = words.pop();
+    console.log(`words: ${chunk.length}`);
+})
